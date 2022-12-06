83309f53922321038d55ec624e55e11d88e8c08e
Guangyuan(Frank) Li <frankli296@yahoo.com>
2022-12-05 22:53:31 -0500

    get stuck at storing very long string

--- altanalyze3/components/io/io_utils.py
+++ altanalyze3/components/io/io_utils.py
@@ -2,6 +2,7 @@ import os,sys
 import pandas as pd
 import numpy as np
 import h5py
+import bisect
 
 
 '''
@@ -45,41 +46,57 @@ class AltAnalyzeIOHandler(object):
 
     @staticmethod
     def compute_optimal_fixed_length(data):
+        # if executing this function, then dtype must be string
         max_length = max([len(item) for item in data])
         return max_length
 
-    def custom_write(self,key,data,categorical=None,dtype=None):
-        if categorical is None:
-            if len(np.unique(data)) < 2**16:  # can be encoded using int16
-                categorical = True
-            else:
-                categorical = False
+    @staticmethod
+    def replace_nan(data,dtype):
+        if dtype == 'string':
+            data = np.array(['no_content' if item is np.nan else item for item in data])
+        elif dtype == 'number':
+            data = np.nan_to_num(data,nan=-1)
+        return data
 
-        if categorical:
-            category2code = {}
-            categories = []
-            for i,v in enumerate(np.unique(data)):  # np unique guarantee to be in ascending lexicographic order
-                category2code[v] = i
-                categories.append(v)
-            codes = np.array([category2code[item] for item in data])
-            categories = np.array(categories)
-
-            group = self.f.create_group(key)
-            group.create_dataset('codes',data=codes.astype('|i2'))  # |i1 = int8, |i2 = int16
-            if dtype is None:
-                auto_inferred_dtype = data.dtype
-                if auto_inferred_dtype.kind == 'O':  # numpy Object
-                    optimal_fixed_length = AltAnalyzeIOHandler.compute_optimal_fixed_length(data)
-                    dtype = '|S{}'.format(optimal_fixed_length)
-            group.create_dataset('categories',data=categories.astype(dtype))
-
+    @staticmethod
+    def compute_n_byte_needed(categories):
+        n = len(categories)
+        if n <= 2**(1*8):
+            n_byte_needed = 1
+        elif n > 2**(1*8) and n <= 2**(2*8):
+            n_byte_needed = 2
+        elif n > 2**(2*8) and n <= 2**(4*8):
+            n_byte_needed = 4
+        elif n > 2**(4*8) and n <= 2**(8*8):
+            n_byte_needed = 8
         else:
-            if dtype is None:
-                auto_inferred_dtype = data.dtype
-                if auto_inferred_dtype.kind == 'O':  # numpy Object
-                    optimal_fixed_length = AltAnalyzeIOHandler.compute_optimal_fixed_length(data)
-                    dtype = '|S{}'.format(optimal_fixed_length)
-            self.f.create_dataset(key,data=data.astype(dtype))
+            raise Exception('number of categories can not be encoded using 2**64, reconsider categorical argument')
+        return n_byte_needed
+
+
+    def write(self,key,data,categorical,dtype):
+        data = AltAnalyzeIOHandler.replace_nan(data,dtype)
+        if dtype == 'string':
+            if categorical:
+                category2code = {}
+                categories = []
+                for i,v in enumerate(np.unique(data)):  # np unique guarantee to be in ascending lexicographic order
+                    category2code[v] = i
+                    categories.append(v)
+                codes = np.array([category2code[item] for item in data])
+                categories = np.array(categories)
+                n_byte_needed = AltAnalyzeIOHandler.compute_n_byte_needed(categories)
+
+                group = self.f.create_group(key)
+                group.create_dataset('codes',data=codes.astype('|i{}'.format(n_byte_needed)))  # |i1 = int8, |i2 = int16, |i3 = int32, |i4 = int64
+                optimal_fixed_length = AltAnalyzeIOHandler.compute_optimal_fixed_length(data)
+                dtype = '|S{}'.format(optimal_fixed_length)
+                group.create_dataset('categories',data=categories.astype(dtype))
+
+            else:
+                optimal_fixed_length = AltAnalyzeIOHandler.compute_optimal_fixed_length(data)
+                dtype = '|S{}'.format(optimal_fixed_length)
+                self.f.create_dataset(key,data=data)
 
 
     def view(self):
--- altanalyze3/components/io/run.py
+++ altanalyze3/components/io/run.py
@@ -7,9 +7,11 @@ import h5py
 
 
 # count_matrix = pd.read_csv('/Users/ligk2e/Desktop/tmp/io/counts.TCGA-SKCM.txt',sep='\t',index_col=0)
-psi_matrix = pd.read_csv('/Users/ligk2e/Desktop/tmp/io/Hs_RNASeq_top_alt_junctions-PSI_EventAnnotation.txt',sep='\t',index_col=0)
-a = psi_matrix.index.values
-print(a.dtype.kind)
+psi_matrix = pd.read_csv('/Users/ligk2e/Desktop/tmp/io/Hs_RNASeq_top_alt_junctions-PSI_EventAnnotation.txt',sep='\t')
+columns = ['Symbol','Description','Examined-Junction','Background-Major-Junction','AltExons','ProteinPredictions','dPSI','ClusterID','UID','Coordinates','EventAnnotation']
+key = 'ProteinPredictions'
+a = psi_matrix[key]
+a.to_csv('test.txt',sep='\t',index=None,header=None);sys.exit('stop')
 
 # with h5py.File('test.h5','w') as f:
 #     f.create_dataset('index',data=a.astype('|S25'))
@@ -17,7 +19,7 @@ print(a.dtype.kind)
 
 # start to test
 with io.AltAnalyzeIOHandler('./test_output.h5','w') as handler:
-    handler.custom_write(key='index',data=a,categorical=None,dtype=None)
+    handler.write(key=key,data=a,categorical=False,dtype='string')
     handler.view()
 
 
